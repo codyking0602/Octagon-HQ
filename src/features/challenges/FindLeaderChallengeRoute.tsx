8a75b093cab9abf634d7af8a87566e8b358205e1
codyking0602 <bcking06@gmail.com>
2026-07-24 16:14:40 -0500

    Support shared match routes for Find the Leader

--- src/features/challenges/FindLeaderChallengeRoute.tsx
+++ src/features/challenges/FindLeaderChallengeRoute.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import PlayPage from "../play/PlayPage";
 import { usePlayChallenges } from "./ChallengeProvider";
 
 export default function FindLeaderChallengeRoute() {
   const location = useLocation();
   const { activeProfile, getChallenge, markOpened } = usePlayChallenges();
-  const challengeCode = new URLSearchParams(location.search).get("challenge")?.toUpperCase() ?? "";
+  const searchParams = new URLSearchParams(location.search);
+  const matchCode = searchParams.get("match")?.toUpperCase() ?? "";
+  const legacyCode = searchParams.get("challenge")?.toUpperCase() ?? "";
+  const challengeCode = matchCode || legacyCode;
   const challenge = challengeCode ? getChallenge(challengeCode) : null;
 
   useEffect(() => {
@@ -15,10 +18,16 @@ export default function FindLeaderChallengeRoute() {
       && activeProfile?.id === challenge.recipientId
       && !challenge.openedAt
       && !challenge.completedAt
+      && !challenge.declinedAt
     ) {
       markOpened(challenge.code);
     }
   }, [activeProfile?.id, challenge, markOpened]);
+
+  if (matchCode && !legacyCode) {
+    searchParams.set("challenge", matchCode);
+    return <Navigate replace to={`${location.pathname}?${searchParams.toString()}`} />;
+  }
 
   return <PlayPage />;
 }
