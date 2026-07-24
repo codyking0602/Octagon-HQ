56251e9cd9abadc7a96693ffd14be77033928f55
codyking0602 <bcking06@gmail.com>
2026-07-24 08:28:02 -0500

    Read Wavelength challenge links

--- src/features/play/WavelengthPage.tsx
+++ src/features/play/WavelengthPage.tsx
@@ -1,7 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import WavelengthGame from "./WavelengthGame";
 
 export default function WavelengthPage() {
+  const location = useLocation();
   const navigate = useNavigate();
-  return <WavelengthGame onExit={() => navigate("/play")} />;
+  const challengeSeed = useMemo(() => {
+    const value = new URLSearchParams(location.search).get("challenge") ?? "";
+    return /^[a-z0-9-]{3,80}$/i.test(value) ? value : undefined;
+  }, [location.search]);
+
+  return <WavelengthGame challengeSeed={challengeSeed} onExit={() => navigate("/play")} />;
 }
