806d9d783740a9313383c901dfc021ad3a657c4c
codyking0602 <bcking06@gmail.com>
2026-07-23 21:29:26 -0500

    Load Intelligence styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/tokens.css";
 import "./styles/global.css";
+import "./styles/intelligence.css";
 
 const root = document.getElementById("root");
 
