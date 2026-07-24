581eed993f2f66ee37eb46af937c665f9642580d
codyking0602 <bcking06@gmail.com>
2026-07-24 14:59:26 -0500

    Load challenge comparison styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./styles/game-results.css";
 import "./styles/blind-games.css";
 import "./styles/final-play-games.css";
 import "./styles/challenge-center.css";
+import "./styles/challenge-results.css";
 
 const root = document.getElementById("root");
 
