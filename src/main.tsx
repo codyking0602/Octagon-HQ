429b41dc2cd21780a6bfe17f2ade8682af93f473
codyking0602 <bcking06@gmail.com>
2026-07-24 07:02:04 -0500

    Load Play feature styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app/App";
 import "./styles/tokens.css";
 import "./styles/global.css";
 import "./styles/intelligence.css";
+import "./styles/play.css";
 
 const root = document.getElementById("root");
 
