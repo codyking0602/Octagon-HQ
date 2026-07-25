5cfd2d47c0b5221cc98e17904769f1ca8979d299
codyking0602 <bcking06@gmail.com>
2026-07-24 21:28:31 -0500

    Load the Your HQ card styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/tokens.css";
 import "./styles/global.css";
+import "./styles/home.css";
 import "./styles/intelligence.css";
 import "./styles/identity.css";
 import "./styles/play.css";
