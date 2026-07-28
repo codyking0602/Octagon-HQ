a645cba714c763c7508c49b124cd683d05529659
codyking0602 <bcking06@gmail.com>
2026-07-28 17:57:54 -0500

    Load the Picks poster override

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/home.css";
 import "./styles/picks.css";
 import "./styles/picks-lifecycle.css";
 import "./styles/picks-polish.css";
+import "./styles/picks-poster.css";
 import "./styles/picks-control.css";
 import "./styles/picks-setup.css";
 import "./styles/picks-setup-source.css";
