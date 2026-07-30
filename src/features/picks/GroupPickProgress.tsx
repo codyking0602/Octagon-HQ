7f6243409aadcb34bd472868d4f23e9396449553
codyking0602 <bcking06@gmail.com>
2026-07-30 17:23:40 -0500

    Use the group progress contract

--- src/features/picks/GroupPickProgress.tsx
+++ src/features/picks/GroupPickProgress.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from "react";
-import type { PickBout, PickEventMemberProgress } from "./picksModel";
+import type { PickBout } from "./picksModel";
+import type { PickEventMemberProgress } from "./groupProgressModel";
 
 interface GroupPickProgressProps {
   members: readonly PickEventMemberProgress[];
