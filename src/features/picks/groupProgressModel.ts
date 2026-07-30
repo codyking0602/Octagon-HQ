7f33ff6e52a8e8aa9ebe2d7e715ca6152d5bbc6b
codyking0602 <bcking06@gmail.com>
2026-07-30 17:34:26 -0500

    Build expandable Picks group progress
    * Add expandable Picks group progress
    
    * Style Picks group progress
    
    * Define Picks group progress contract
    
    * Use the group progress contract

---
+++ src/features/picks/groupProgressModel.ts
@@ -0,0 +1,8 @@
+export interface PickEventMemberProgress {
+  profileId: string;
+  displayName: string;
+  completed: number;
+  total: number;
+  hasUnderdogLock: boolean;
+  isCurrentUser: boolean;
+}
