a1f5b632195d4ef882011b66cac373ec5a87aed4
codyking0602 <bcking06@gmail.com>
2026-08-03 19:25:48 -0500

    Keep Auction release from changing global scroll behavior

--- src/app/RouteScrollManager.tsx
+++ src/app/RouteScrollManager.tsx
@@ -20,7 +20,7 @@ export function RouteScrollManager() {
   useEffect(() => {
     if (location.hash) return;
     scrollPageToTop();
-  }, [location.hash, location.pathname, location.search]);
+  }, [location.hash, location.pathname]);
 
   return null;
 }
