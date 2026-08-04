1c44e50e8bc503a3a7869afaf319aaccbf1f45e4
codyking0602 <bcking06@gmail.com>
2026-08-03 22:18:46 -0500

    Map Auction modes to sprite views

--- src/features/play/auctionModeArtwork.ts
+++ src/features/play/auctionModeArtwork.ts
@@ -5,71 +5,32 @@ export interface AuctionModeArtwork {
   objectPosition: string;
 }
 
+const sprite = "/auction/auction-format-sprite.svg";
+
+function spriteView(x: number, y: number): AuctionModeArtwork {
+  return {
+    src: `${sprite}#svgView(viewBox(${x},${y},180,101))`,
+    objectPosition: "50% 50%",
+  };
+}
+
 export const auctionModeArtworks = {
-  "ultimate-fighter": {
-    src: "/auction/ultimate-fighter.svg",
-    objectPosition: "50% 50%",
-  },
-  "jon-jones-performances": {
-    src: "/auction/jon-jones-performances.svg",
-    objectPosition: "50% 42%",
-  },
-  "conor-mcgregor-performances": {
-    src: "/auction/conor-mcgregor-performances.svg",
-    objectPosition: "50% 45%",
-  },
-  "charles-oliveira-performances": {
-    src: "/auction/charles-oliveira-performances.svg",
-    objectPosition: "50% 38%",
-  },
-  "fighter-performances": {
-    src: "/auction/fighter-performances.svg",
-    objectPosition: "50% 50%",
-  },
-  strikers: {
-    src: "/auction/strikers.svg",
-    objectPosition: "50% 50%",
-  },
-  grapplers: {
-    src: "/auction/grapplers.svg",
-    objectPosition: "50% 54%",
-  },
-  "knockout-artists": {
-    src: "/auction/knockout-artists.svg",
-    objectPosition: "50% 50%",
-  },
-  "greatest-ufc-card": {
-    src: "/auction/greatest-ufc-card.svg",
-    objectPosition: "50% 50%",
-  },
-  "championship-performances": {
-    src: "/auction/championship-performances.svg",
-    objectPosition: "50% 50%",
-  },
-  finishes: {
-    src: "/auction/finishes.svg",
-    objectPosition: "50% 50%",
-  },
-  "dominant-performances": {
-    src: "/auction/dominant-performances.svg",
-    objectPosition: "50% 50%",
-  },
-  wars: {
-    src: "/auction/wars.svg",
-    objectPosition: "50% 50%",
-  },
-  rivalries: {
-    src: "/auction/rivalries.svg",
-    objectPosition: "50% 50%",
-  },
-  "iconic-moments": {
-    src: "/auction/iconic-moments.svg",
-    objectPosition: "50% 50%",
-  },
-  nicknames: {
-    src: "/auction/nicknames.svg",
-    objectPosition: "50% 42%",
-  },
+  "ultimate-fighter": spriteView(0, 0),
+  "jon-jones-performances": spriteView(180, 0),
+  "conor-mcgregor-performances": spriteView(360, 0),
+  "charles-oliveira-performances": spriteView(540, 0),
+  "fighter-performances": spriteView(0, 101),
+  strikers: spriteView(180, 101),
+  grapplers: spriteView(360, 101),
+  "knockout-artists": spriteView(540, 101),
+  "greatest-ufc-card": spriteView(0, 202),
+  "championship-performances": spriteView(180, 202),
+  finishes: spriteView(360, 202),
+  "dominant-performances": spriteView(540, 202),
+  wars: spriteView(0, 303),
+  rivalries: spriteView(180, 303),
+  "iconic-moments": spriteView(360, 303),
+  nicknames: spriteView(540, 303),
 } as const satisfies Readonly<Record<AuctionModeId, AuctionModeArtwork>>;
 
 export function auctionModeArtwork(modeId: AuctionModeId): AuctionModeArtwork {
