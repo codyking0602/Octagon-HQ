de9c5a599b8b8c3721d12aa3ce48a72c2ca2df67
codyking0602 <bcking06@gmail.com>
2026-08-05 10:25:47 -0500

    Load Daily Challenge Standings styles

--- src/main.tsx
+++ src/main.tsx
@@ -33,6 +33,7 @@ import "./styles/member-profile-push.css";
 import "./styles/play.css";
 import "./styles/today-challenge.css";
 import "./styles/today-challenge-hub.css";
+import "./styles/daily-challenge-standings.css";
 import "./styles/auction.css";
 import "./styles/find-leader-leaderboard.css";
 import "./styles/wavelength.css";
