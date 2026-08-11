2e1f8eb4982cb7497564a591ddf99434e4ceede7
codyking0602 <bcking06@gmail.com>
2026-08-11 07:59:42 -0500

    Use canonical Picks poster sizing

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,6 @@ import "./styles/picks-season-hub.css";
 import "./styles/picks-event-recap.css";
 import "./styles/picks-group-progress.css";
 import "./styles/picks-polish.css";
-import "./styles/picks-ufc330-poster.css";
 import "./styles/picks-owner-entry.css";
 import "./styles/picks-control.css";
 import "./styles/picks-control-history.css";
