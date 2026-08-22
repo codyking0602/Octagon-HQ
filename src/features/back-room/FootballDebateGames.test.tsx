7008c7d5d9905a3b5853ffcd82eb8f32770c8de5
codyking0602 <bcking06@gmail.com>
2026-08-22 09:42:35 -0500

    Keep Football debate tests aligned with PR3 packs

--- src/features/back-room/FootballDebateGames.test.tsx
+++ src/features/back-room/FootballDebateGames.test.tsx
@@ -13,7 +13,7 @@ import {
   footballWavelengthClues,
   nextFootballWavelengthClue,
 } from "./footballWavelengthModel";
-import { getFootballRankFivePack } from "./footballRankFiveModel";
+import { footballRankFivePacks, getFootballRankFivePack } from "./footballRankFiveModel";
 
 describe("Football Back Room debate games", () => {
   beforeEach(() => {
@@ -21,7 +21,7 @@ describe("Football Back Room debate games", () => {
   });
 
   it("reuses the Football Rank 5 item owner for all Keep 4 / Cut 4 rooms", () => {
-    expect(footballKeepCutPacks).toHaveLength(7);
+    expect(footballKeepCutPacks).toHaveLength(footballRankFivePacks.length);
     for (const pack of footballKeepCutPacks) {
       expect(pack.items).toBe(getFootballRankFivePack(pack.id).items);
       const first = buildFootballKeepCutLineup(pack.id, "keep-cut-proof");
