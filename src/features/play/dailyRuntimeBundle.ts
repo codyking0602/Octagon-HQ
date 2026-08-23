0fd085b6c7d5dc05a5cce63beadfa33e9b7b845e
codyking0602 <bcking06@gmail.com>
2026-08-22 22:32:19 -0500

    Bundle shared UFC and Football daily runtimes

---
+++ src/features/play/dailyRuntimeBundle.ts
@@ -0,0 +1,6 @@
+export {
+  advanceOfficialDailyRuntime,
+  buildOfficialDailySetup,
+} from "./todaysChallengeRuntime";
+
+export { buildFootballTodayProjection } from "./footballTodayChallengeSession";
