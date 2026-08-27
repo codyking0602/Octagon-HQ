0b66f27a11ca6ee7e16b51a86504aaa113a7ac06
codyking0602 <bcking06@gmail.com>
2026-08-27 08:15:53 -0500

    Route Blind Resume evidence through ledger eligibility

--- src/features/back-room/footballFactualStats.ts
+++ src/features/back-room/footballFactualStats.ts
@@ -1,6 +1,17 @@
 export * from "./footballFactualStatsCore";
 export * from "./footballFactualStatsCatalog";
-export * from "./footballBlindResumeEvidence";
+export {
+  footballBlindResumeEvidenceProfiles,
+  validateFootballBlindResumeEvidenceProfile,
+  type FootballBlindResumeArchetype,
+  type FootballBlindResumeEvidenceProfile,
+  type FootballBlindResumeEvidenceRow,
+  type FootballBlindResumeEvidenceValueTuple,
+} from "./footballBlindResumeEvidence";
+export {
+  getFootballBlindResumeEvidenceProfile,
+  getFootballBlindResumeEvidenceProfilesForPack,
+} from "./footballBlindResumeLedgerEvidence";
 export * from "./footballSubjectRegistry";
 export * from "./footballSubjectEligibility";
 export { footballFindLeaderLeagueForDomain } from "./footballFindLeaderStats";
