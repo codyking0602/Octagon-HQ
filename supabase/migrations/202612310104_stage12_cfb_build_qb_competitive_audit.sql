0e4c3de43394babfa2fe0432914fc9d471bf8a4d
codyking0602 <bcking06@gmail.com>
2026-09-13 03:44:52 -0500

    Stage 12: synchronize exact CFB peak identities

--- supabase/migrations/202612310104_stage12_cfb_build_qb_competitive_audit.sql
+++ supabase/migrations/202612310104_stage12_cfb_build_qb_competitive_audit.sql
@@ -135,6 +135,100 @@ where catalog.content_version = 'football-draft-room-2026-09-v4'
   and catalog.mode_id = 'build-qb-cfb'
   and catalog.item_reference = calibrated.item_reference;
 
+
+with exact_identity (
+  item_reference, peak_season, school, canonical_player_id
+) as (
+  values
+    ('cfb-build-qb-cam-newton-2010', 2010, 'Auburn', 'cfb-cam-newton'),
+    ('cfb-build-qb-vince-young-2005', 2005, 'Texas', 'cfb-vince-young'),
+    ('cfb-build-qb-tim-tebow-2007', 2007, 'Florida', 'cfb-tim-tebow'),
+    ('cfb-build-qb-matt-leinart-2004', 2004, 'USC', 'cfb-matt-leinart'),
+    ('cfb-build-qb-johnny-manziel-2012', 2012, 'Texas A&M', 'cfb-johnny-manziel'),
+    ('cfb-build-qb-colt-mccoy-2008', 2008, 'Texas', 'cfb-colt-mccoy'),
+    ('cfb-build-qb-sam-bradford-2008', 2008, 'Oklahoma', 'cfb-sam-bradford'),
+    ('cfb-build-qb-jameis-winston-2013', 2013, 'Florida State', 'cfb-jameis-winston'),
+    ('cfb-build-qb-robert-griffin-iii-2011', 2011, 'Baylor', 'cfb-robert-griffin-iii'),
+    ('cfb-build-qb-doug-flutie-1984', 1984, 'Boston College', 'cfb-doug-flutie'),
+    ('cfb-build-qb-vinny-testaverde-1986', 1986, 'Miami', 'cfb-vinny-testaverde'),
+    ('cfb-build-qb-charlie-ward-1993', 1993, 'Florida State', 'cfb-charlie-ward'),
+    ('cfb-build-qb-danny-wuerffel-1996', 1996, 'Florida', 'cfb-danny-wuerffel'),
+    ('cfb-build-qb-ty-detmer-1990', 1990, 'BYU', 'cfb-ty-detmer'),
+    ('cfb-build-qb-gino-torretta-1992', 1992, 'Miami', 'cfb-gino-torretta'),
+    ('cfb-build-qb-andre-ware-1989', 1989, 'Houston', 'cfb-andre-ware'),
+    ('cfb-build-qb-eric-crouch-2001', 2001, 'Nebraska', 'cfb-eric-crouch'),
+    ('cfb-build-qb-jim-plunkett-1970', 1970, 'Stanford', 'cfb-jim-plunkett'),
+    ('cfb-build-qb-roger-staubach-1963', 1963, 'Navy', 'cfb-roger-staubach'),
+    ('cfb-build-qb-troy-smith-2006', 2006, 'Ohio State', 'cfb-troy-smith'),
+    ('cfb-build-qb-brady-quinn-2005', 2005, 'Notre Dame', 'cfb-brady-quinn'),
+    ('cfb-build-qb-carson-palmer-2002', 2002, 'USC', 'cfb-carson-palmer'),
+    ('cfb-build-qb-chris-weinke-2000', 2000, 'Florida State', 'cfb-chris-weinke'),
+    ('cfb-build-qb-jason-white-2003', 2003, 'Oklahoma', 'cfb-jason-white'),
+    ('cfb-build-qb-marcus-mariota-2014', 2014, 'Oregon', 'cfbfast-r-player-511459-marcus-mariota'),
+    ('cfb-build-qb-dak-prescott-2014', 2014, 'Mississippi State', 'cfbfast-r-player-512030-dak-prescott'),
+    ('cfb-build-qb-jared-goff-2015', 2015, 'California', 'cfbfast-r-player-547401-jared-goff'),
+    ('cfb-build-qb-patrick-mahomes-2016', 2016, 'Texas Tech', 'cfbfast-r-player-3139477-patrick-mahomes'),
+    ('cfb-build-qb-deshaun-watson-2016', 2016, 'Clemson', 'cfbfast-r-player-3122840-deshaun-watson'),
+    ('cfb-build-qb-lamar-jackson-2016', 2016, 'Louisville', 'cfbfast-r-player-3916387-lamar-jackson'),
+    ('cfb-build-qb-baker-mayfield-2017', 2017, 'Oklahoma', 'cfbfast-r-player-550373-baker-mayfield'),
+    ('cfb-build-qb-josh-allen-2016', 2016, 'Wyoming', 'cfbfast-r-player-3918298-josh-allen'),
+    ('cfb-build-qb-drew-lock-2017', 2017, 'Missouri', 'cfbfast-r-player-3924327-drew-lock'),
+    ('cfb-build-qb-mason-rudolph-2017', 2017, 'Oklahoma State', 'cfbfast-r-player-3116407-mason-rudolph'),
+    ('cfb-build-qb-sam-darnold-2016', 2016, 'USC', 'cfbfast-r-player-3912547-sam-darnold'),
+    ('cfb-build-qb-gardner-minshew-2018', 2018, 'Washington State', 'cfbfast-r-player-4038524-gardner-minshew'),
+    ('cfb-build-qb-kyler-murray-2018', 2018, 'Oklahoma', 'cfbfast-r-player-3917315-kyler-murray'),
+    ('cfb-build-qb-tua-tagovailoa-2018', 2018, 'Alabama', 'cfbfast-r-player-4241479-tua-tagovailoa'),
+    ('cfb-build-qb-trevor-lawrence-2019', 2019, 'Clemson', 'cfbfast-r-player-4360310-trevor-lawrence'),
+    ('cfb-build-qb-joe-burrow-2019', 2019, 'LSU', 'cfbfast-r-player-3915511-joe-burrow'),
+    ('cfb-build-qb-jalen-hurts-2019', 2019, 'Oklahoma', 'cfbfast-r-player-4040715-jalen-hurts'),
+    ('cfb-build-qb-justin-fields-2019', 2019, 'Ohio State', 'cfbfast-r-player-4362887-justin-fields'),
+    ('cfb-build-qb-justin-herbert-2019', 2019, 'Oregon', 'cfbfast-r-player-4038941-justin-herbert'),
+    ('cfb-build-qb-jordan-love-2018', 2018, 'Utah State', 'cfbfast-r-player-4036378-jordan-love'),
+    ('cfb-build-qb-mac-jones-2020', 2020, 'Alabama', 'cfbfast-r-player-4241464-mac-jones'),
+    ('cfb-build-qb-kyle-trask-2020', 2020, 'Florida', 'cfbfast-r-player-4034946-kyle-trask'),
+    ('cfb-build-qb-zach-wilson-2020', 2020, 'BYU', 'cfbfast-r-player-4361259-zach-wilson'),
+    ('cfb-build-qb-sam-howell-2020', 2020, 'North Carolina', 'cfbfast-r-player-4426875-sam-howell'),
+    ('cfb-build-qb-spencer-rattler-2020', 2020, 'Oklahoma', 'cfbfast-r-player-4426339-spencer-rattler'),
+    ('cfb-build-qb-brock-purdy-2020', 2020, 'Iowa State', 'cfbfast-r-player-4361741-brock-purdy'),
+    ('cfb-build-qb-bryce-young-2021', 2021, 'Alabama', 'cfbfast-r-player-4685720-bryce-young'),
+    ('cfb-build-qb-c-j-stroud-2021', 2021, 'Ohio State', 'cfbfast-r-player-4432577-c-j-stroud'),
+    ('cfb-build-qb-kenny-pickett-2021', 2021, 'Pittsburgh', 'cfbfast-r-player-4240703-kenny-pickett'),
+    ('cfb-build-qb-bailey-zappe-2021', 2021, 'Western Kentucky', 'cfbfast-r-player-4250360-bailey-zappe'),
+    ('cfb-build-qb-matt-corral-2021', 2021, 'Ole Miss', 'cfbfast-r-player-4362874-matt-corral'),
+    ('cfb-build-qb-malik-willis-2021', 2021, 'Liberty', 'cfbfast-r-player-4242512-malik-willis'),
+    ('cfb-build-qb-sam-hartman-2021', 2021, 'Wake Forest', 'cfbfast-r-player-4361994-sam-hartman'),
+    ('cfb-build-qb-caleb-williams-2022', 2022, 'USC', 'cfbfast-r-player-4431611-caleb-williams'),
+    ('cfb-build-qb-max-duggan-2022', 2022, 'TCU', 'cfbfast-r-player-4427105-max-duggan'),
+    ('cfb-build-qb-drake-maye-2022', 2022, 'North Carolina', 'cfbfast-r-player-4431452-drake-maye'),
+    ('cfb-build-qb-hendon-hooker-2022', 2022, 'Tennessee', 'cfbfast-r-player-4240858-hendon-hooker'),
+    ('cfb-build-qb-stetson-bennett-2022', 2022, 'Georgia', 'cfbfast-r-player-4259553-stetson-bennett'),
+    ('cfb-build-qb-michael-penix-jr-2023', 2023, 'Washington', 'cfbfast-r-player-4360423-michael-penix-jr'),
+    ('cfb-build-qb-bo-nix-2023', 2023, 'Oregon', 'cfbfast-r-player-4426338-bo-nix'),
+    ('cfb-build-qb-jayden-daniels-2023', 2023, 'LSU', 'cfbfast-r-player-4426348-jayden-daniels'),
+    ('cfb-build-qb-j-j-mccarthy-2023', 2023, 'Michigan', 'cfbfast-r-player-4433970-j-j-mccarthy'),
+    ('cfb-build-qb-jordan-travis-2023', 2023, 'Florida State', 'cfbfast-r-player-4360799-jordan-travis'),
+    ('cfb-build-qb-quinn-ewers-2023', 2023, 'Texas', 'cfbfast-r-player-4889929-quinn-ewers'),
+    ('cfb-build-qb-dillon-gabriel-2024', 2024, 'Oregon', 'cfbfast-r-player-4427238-dillon-gabriel'),
+    ('cfb-build-qb-shedeur-sanders-2024', 2024, 'Colorado', 'cfbfast-r-player-4432762-shedeur-sanders'),
+    ('cfb-build-qb-cameron-ward-2024', 2024, 'Miami', 'cfbfast-r-player-4688380-cameron-ward'),
+    ('cfb-build-qb-jaxson-dart-2024', 2024, 'Ole Miss', 'cfbfast-r-player-4689114-jaxson-dart'),
+    ('cfb-build-qb-will-howard-2024', 2024, 'Ohio State', 'cfbfast-r-player-4429955-will-howard'),
+    ('cfb-build-qb-kyle-mccord-2024', 2024, 'Syracuse', 'cfbfast-r-player-4433971-kyle-mccord'),
+    ('cfb-build-qb-jalen-milroe-2023', 2023, 'Alabama', 'cfbfast-r-player-4432734-jalen-milroe'),
+    ('cfb-build-qb-carson-beck-2023', 2023, 'Georgia', 'cfbfast-r-player-4430841-carson-beck'),
+    ('cfb-build-qb-riley-leonard-2024', 2024, 'Notre Dame', 'cfbfast-r-player-4683423-riley-leonard'),
+    ('cfb-build-qb-kaidon-salter-2023', 2023, 'Liberty', 'cfbfast-r-player-4432803-kaidon-salter'),
+    ('cfb-build-qb-grayson-mccall-2021', 2021, 'Coastal Carolina', 'cfbfast-r-player-4427936-grayson-mccall'),
+    ('cfb-build-qb-deriq-king-2018', 2018, 'Houston', 'cfbfast-r-player-4039300-d-eriq-king')
+)
+update private.auction_catalog as catalog
+set display_description =
+  exact_identity.peak_season::text || ' ' || exact_identity.school
+  || ' peak-season CFB QB profile · ' || exact_identity.canonical_player_id
+from exact_identity
+where catalog.content_version = 'football-draft-room-2026-09-v4'
+  and catalog.mode_id = 'build-qb-cfb'
+  and catalog.item_reference = exact_identity.item_reference;
 create or replace function private.grade_auction(p_auction_id uuid)
 returns void
 language plpgsql
