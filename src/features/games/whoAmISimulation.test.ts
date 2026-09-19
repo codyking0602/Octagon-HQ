7b47629a061c0a980a0e85a696bbb7df7fa40fb8
codyking0602 <bcking06@gmail.com>
2026-09-18 21:10:58 -0500

    Allow full Who Am I simulation to complete

--- src/features/games/whoAmISimulation.test.ts
+++ src/features/games/whoAmISimulation.test.ts
@@ -344,5 +344,5 @@ describe("Who Am I mature whole-game simulation", () => {
 
     expect(allFindings).toHaveLength(500);
     expect(allFindings.filter((finding) => Number(finding.candidateClues) < WHO_AM_I_CLUE_LIMIT)).toEqual([]);
-  }, 150_000);
+  }, 300_000);
 });
