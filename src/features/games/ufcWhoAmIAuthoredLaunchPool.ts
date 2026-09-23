d1c41552db6c7d8f0aea9b40a0b038ea5cebb31a
codyking0602 <bcking06@gmail.com>
2026-09-22 23:05:44 -0500

    Who Am I: combine full 100-fighter UFC authored pool

--- src/features/games/ufcWhoAmIAuthoredLaunchPool.ts
+++ src/features/games/ufcWhoAmIAuthoredLaunchPool.ts
@@ -1,9 +1,25 @@
 import { ufcWhoAmIAuthoredIdentities } from "./ufcWhoAmIAuthoredScripts";
 import { ufcWhoAmIAuthoredLaunchExpansion } from "./ufcWhoAmIAuthoredLaunchExpansion";
+import { ufcWhoAmIAuthoredBatch1 } from "./ufcWhoAmIAuthoredBatch1";
+import { ufcWhoAmIAuthoredBatch2 } from "./ufcWhoAmIAuthoredBatch2";
+import { ufcWhoAmIAuthoredBatch3 } from "./ufcWhoAmIAuthoredBatch3";
+import { ufcWhoAmIAuthoredBatch4 } from "./ufcWhoAmIAuthoredBatch4";
+import { ufcWhoAmIAuthoredBatch5 } from "./ufcWhoAmIAuthoredBatch5";
+import { ufcWhoAmIAuthoredBatch6 } from "./ufcWhoAmIAuthoredBatch6";
+import { ufcWhoAmIAuthoredBatch7 } from "./ufcWhoAmIAuthoredBatch7";
+import { ufcWhoAmIAuthoredBatch8 } from "./ufcWhoAmIAuthoredBatch8";
 
 export const ufcWhoAmIAuthoredLaunchPool = [
   ...ufcWhoAmIAuthoredIdentities,
   ...ufcWhoAmIAuthoredLaunchExpansion,
+  ...ufcWhoAmIAuthoredBatch1,
+  ...ufcWhoAmIAuthoredBatch2,
+  ...ufcWhoAmIAuthoredBatch3,
+  ...ufcWhoAmIAuthoredBatch4,
+  ...ufcWhoAmIAuthoredBatch5,
+  ...ufcWhoAmIAuthoredBatch6,
+  ...ufcWhoAmIAuthoredBatch7,
+  ...ufcWhoAmIAuthoredBatch8,
 ] as const;
 
 const bySubjectId = new Map(
