c8a80bd521aec4e4b2f6265e5bcb2dcd06c72d82
codyking0602 <bcking06@gmail.com>
2026-09-23 20:27:01 -0500

    Who Am I: add canonical UFC authored expansion

---
+++ src/features/back-room/ufcWhoAmIAuthoredCanonicalExpansion.ts
@@ -0,0 +1,388 @@
+/**
+ * Canonical identity-only UFC subjects added for the authored Who Am I roster.
+ *
+ * These records intentionally do not pretend to be complete UFC fight ledgers.
+ * Authored clues carry their own verified source provenance; the legacy generated
+ * UFC Who Am I path remains pinned to the original factual-fight population.
+ */
+export const ufcWhoAmIAuthoredCanonicalExpansion = [
+  {
+    "id": "ufc:jiri-prochazka",
+    "name": "Jiří Procházka",
+    "slug": "jiri-prochazka",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2020-07-11",
+    "activeTo": "2026-04-11",
+    "identitySourceUrl": "https://www.ufc.com/athlete/jiri-prochazka"
+  },
+  {
+    "id": "ufc:magomed-ankalaev",
+    "name": "Magomed Ankalaev",
+    "slug": "magomed-ankalaev",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2018-03-17",
+    "activeTo": "2026-07-25",
+    "identitySourceUrl": "https://www.ufc.com/athlete/magomed-ankalaev"
+  },
+  {
+    "id": "ufc:jan-blachowicz",
+    "name": "Jan Błachowicz",
+    "slug": "jan-blachowicz",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2014-10-04",
+    "activeTo": "2026-08-01",
+    "identitySourceUrl": "https://www.ufc.com/athlete/jan-blachowicz"
+  },
+  {
+    "id": "ufc:jamahal-hill",
+    "name": "Jamahal Hill",
+    "slug": "jamahal-hill",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2020-01-25",
+    "activeTo": "2025-06-21",
+    "identitySourceUrl": "https://www.ufc.com/athlete/jamahal-hill"
+  },
+  {
+    "id": "ufc:carlos-ulberg",
+    "name": "Carlos Ulberg",
+    "slug": "carlos-ulberg",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2021-03-06",
+    "activeTo": "2026-04-11",
+    "identitySourceUrl": "https://www.ufc.com/athlete/carlos-ulberg"
+  },
+  {
+    "id": "ufc:arman-tsarukyan",
+    "name": "Arman Tsarukyan",
+    "slug": "arman-tsarukyan",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Lightweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2019-04-20",
+    "activeTo": "2026-09-19",
+    "identitySourceUrl": "https://www.ufc.com/athlete/arman-tsarukyan"
+  },
+  {
+    "id": "ufc:cory-sandhagen",
+    "name": "Cory Sandhagen",
+    "slug": "cory-sandhagen",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Bantamweight",
+    "secondaryDivisions": [
+      "Featherweight"
+    ],
+    "activeFrom": "2018-01-27",
+    "activeTo": "2026-07-11",
+    "identitySourceUrl": "https://www.ufc.com/athlete/cory-sandhagen"
+  },
+  {
+    "id": "ufc:umar-nurmagomedov",
+    "name": "Umar Nurmagomedov",
+    "slug": "umar-nurmagomedov",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Bantamweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2021-01-20",
+    "activeTo": "2026-08-29",
+    "identitySourceUrl": "https://www.ufc.com/athlete/umar-nurmagomedov"
+  },
+  {
+    "id": "ufc:belal-muhammad",
+    "name": "Belal Muhammad",
+    "slug": "belal-muhammad",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2016-07-07",
+    "activeTo": "2026-06-06",
+    "identitySourceUrl": "https://www.ufc.com/athlete/belal-muhammad"
+  },
+  {
+    "id": "ufc:jack-della-maddalena",
+    "name": "Jack Della Maddalena",
+    "slug": "jack-della-maddalena",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2022-01-22",
+    "activeTo": "2026-05-02",
+    "identitySourceUrl": "https://www.ufc.com/athlete/jack-della-maddalena"
+  },
+  {
+    "id": "ufc:ian-machado-garry",
+    "name": "Ian Machado Garry",
+    "slug": "ian-machado-garry",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2021-11-06",
+    "activeTo": "2026-08-15",
+    "identitySourceUrl": "https://www.ufc.com/athlete/ian-machado-garry"
+  },
+  {
+    "id": "ufc:shavkat-rakhmonov",
+    "name": "Shavkat Rakhmonov",
+    "slug": "shavkat-rakhmonov",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2020-10-24",
+    "activeTo": "2024-12-07",
+    "identitySourceUrl": "https://www.ufc.com/athlete/shavkat-rakhmonov"
+  },
+  {
+    "id": "ufc:urijah-faber",
+    "name": "Urijah Faber",
+    "slug": "urijah-faber",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Bantamweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2011-03-19",
+    "activeTo": "2019-12-14",
+    "identitySourceUrl": "https://www.ufc.com/athlete/urijah-faber"
+  },
+  {
+    "id": "ufc:frank-mir",
+    "name": "Frank Mir",
+    "slug": "frank-mir",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2001-11-02",
+    "activeTo": "2016-03-19",
+    "identitySourceUrl": "https://www.ufc.com/athlete/frank-mir"
+  },
+  {
+    "id": "ufc:alistair-overeem",
+    "name": "Alistair Overeem",
+    "slug": "alistair-overeem",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Heavyweight",
+    "secondaryDivisions": [
+      "Light Heavyweight"
+    ],
+    "activeFrom": "2011-12-30",
+    "activeTo": "2021-02-06",
+    "identitySourceUrl": "https://www.ufc.com/athlete/alistair-overeem"
+  },
+  {
+    "id": "ufc:rich-franklin",
+    "name": "Rich Franklin",
+    "slug": "rich-franklin",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Middleweight",
+    "secondaryDivisions": [
+      "Light Heavyweight"
+    ],
+    "activeFrom": "2003-04-25",
+    "activeTo": "2012-11-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/rich-franklin"
+  },
+  {
+    "id": "ufc:carlos-condit",
+    "name": "Carlos Condit",
+    "slug": "carlos-condit",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2009-04-01",
+    "activeTo": "2021-07-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/carlos-condit"
+  },
+  {
+    "id": "ufc:matt-serra",
+    "name": "Matt Serra",
+    "slug": "matt-serra",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [
+      "Lightweight"
+    ],
+    "activeFrom": "2001-05-04",
+    "activeTo": "2010-09-25",
+    "identitySourceUrl": "https://www.ufc.com/athlete/matt-serra"
+  },
+  {
+    "id": "ufc:luke-rockhold",
+    "name": "Luke Rockhold",
+    "slug": "luke-rockhold",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Middleweight",
+    "secondaryDivisions": [
+      "Light Heavyweight"
+    ],
+    "activeFrom": "2013-05-18",
+    "activeTo": "2022-08-20",
+    "identitySourceUrl": "https://www.ufc.com/athlete/luke-rockhold"
+  },
+  {
+    "id": "ufc:yoel-romero",
+    "name": "Yoel Romero",
+    "slug": "yoel-romero",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Middleweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2013-04-20",
+    "activeTo": "2020-03-07",
+    "identitySourceUrl": "https://www.ufc.com/athlete/yoeru-romero"
+  },
+  {
+    "id": "ufc:ken-shamrock",
+    "name": "Ken Shamrock",
+    "slug": "ken-shamrock",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Light Heavyweight",
+    "secondaryDivisions": [
+      "Heavyweight"
+    ],
+    "activeFrom": "1993-11-12",
+    "activeTo": "2006-10-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/ken-shamrock"
+  },
+  {
+    "id": "ufc:joshua-van",
+    "name": "Joshua Van",
+    "slug": "joshua-van",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Flyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2023-06-24",
+    "activeTo": "2026-09-19",
+    "identitySourceUrl": "https://www.ufc.com/athlete/joshua-van"
+  },
+  {
+    "id": "ufc:brandon-royval",
+    "name": "Brandon Royval",
+    "slug": "brandon-royval",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Flyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2020-05-30",
+    "activeTo": "2026-07-11",
+    "identitySourceUrl": "https://www.ufc.com/athlete/brandon-royval"
+  },
+  {
+    "id": "ufc:michael-morales",
+    "name": "Michael Morales",
+    "slug": "michael-morales",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2022-01-22",
+    "activeTo": "2025-11-15",
+    "identitySourceUrl": "https://www.ufc.com/athlete/michael-morales"
+  },
+  {
+    "id": "ufc:carlos-prates",
+    "name": "Carlos Prates",
+    "slug": "carlos-prates",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2024-02-10",
+    "activeTo": "2026-05-02",
+    "identitySourceUrl": "https://www.ufc.com/athlete/carlos-prates"
+  },
+  {
+    "id": "ufc:tim-kennedy",
+    "name": "Tim Kennedy",
+    "slug": "tim-kennedy",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Middleweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2013-07-06",
+    "activeTo": "2016-12-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/tim-kennedy"
+  },
+  {
+    "id": "ufc:brendan-schaub",
+    "name": "Brendan Schaub",
+    "slug": "brendan-schaub",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2009-12-05",
+    "activeTo": "2014-12-06",
+    "identitySourceUrl": "https://www.ufc.com/athlete/brendan-schaub"
+  },
+  {
+    "id": "ufc:kimbo-slice",
+    "name": "Kimbo Slice",
+    "slug": "kimbo-slice",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2009-12-05",
+    "activeTo": "2010-05-08",
+    "identitySourceUrl": "https://www.ufc.com/athlete/kimbo-slice"
+  },
+  {
+    "id": "ufc:cm-punk",
+    "name": "CM Punk",
+    "slug": "cm-punk",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2016-09-10",
+    "activeTo": "2018-06-09",
+    "identitySourceUrl": "https://www.ufc.com/athlete/cm-punk"
+  },
+  {
+    "id": "ufc:mike-perry",
+    "name": "Mike Perry",
+    "slug": "mike-perry",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Welterweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2016-08-20",
+    "activeTo": "2021-04-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/mike-perry"
+  },
+  {
+    "id": "ufc:paige-vanzant",
+    "name": "Paige VanZant",
+    "slug": "paige-vanzant",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Women's Flyweight",
+    "secondaryDivisions": [
+      "Women's Strawweight"
+    ],
+    "activeFrom": "2014-11-22",
+    "activeTo": "2020-07-11",
+    "identitySourceUrl": "https://www.ufc.com/athlete/paige-vanzant"
+  },
+  {
+    "id": "ufc:greg-hardy",
+    "name": "Greg Hardy",
+    "slug": "greg-hardy",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Heavyweight",
+    "secondaryDivisions": [],
+    "activeFrom": "2019-01-19",
+    "activeTo": "2022-03-05",
+    "identitySourceUrl": "https://www.ufc.com/athlete/greg-hardy"
+  },
+  {
+    "id": "ufc:darren-till",
+    "name": "Darren Till",
+    "slug": "darren-till",
+    "recognizabilityTier": "A",
+    "primaryDivision": "Middleweight",
+    "secondaryDivisions": [
+      "Welterweight"
+    ],
+    "activeFrom": "2015-05-30",
+    "activeTo": "2022-12-10",
+    "identitySourceUrl": "https://www.ufc.com/athlete/darren-till"
+  }
+] as const;
