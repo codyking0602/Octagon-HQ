00990ac7b07771c5153c97ca10cf2e8d995cf036
codyking0602 <bcking06@gmail.com>
2026-09-23 19:34:04 -0500

    Who Am I: fix authored Football Casual import

--- src/features/play/FootballWhoAmIPage.tsx
+++ src/features/play/FootballWhoAmIPage.tsx
@@ -2,7 +2,10 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useProfileChallengeMatch } from "../challenges/challengeRuntime";
 import { usePlayChallenges } from "../challenges/ChallengeProvider";
 import type { WhoAmIRound } from "../games/whoAmIEngine";
-import { createFootballWhoAmIRound } from "../games/whoAmIAuthority";
+import {
+  createFootballWhoAmIAuthoredCasualRound,
+  rememberFootballWhoAmIAuthoredCasualRound,
+} from "./footballWhoAmIAuthoredCasual";
 import WhoAmIPage from "./WhoAmIPage";
 import {
   sharedWhoAmIRound,
